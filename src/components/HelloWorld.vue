3091e9372225e81289011af85d9b3a889b0a4bcf
Carles Escrig Royo <esroyo@gmail.com>
2024-02-29 20:05:33 +0100

    pref: add preconnect link

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-  <h3>served over <strong><a href="https://systemjs.sh/vue">systemjs.sh CDN</a></strong></h3>
+  <h3>served over <strong><a href="https://systemjs.sh">systemjs.sh CDN</a></strong></h3>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
